f411202d2c11d1b619eba986f1ac11919eb6ef64
Harshith14hs <harshithhs14@gmail.com>
2025-06-19 23:18:03 +0530

    front

--- src/index.js
+++ src/index.js
@@ -10,11 +10,9 @@ const router=createBrowserRouter([{path:"/",element:<App/>}])
  
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <StoreProvider>
-      <RouterProvider router={router}/>
-    </StoreProvider>
-  </React.StrictMode>
+  <StoreProvider>
+    <RouterProvider router={router}/>
+  </StoreProvider>
 );
 
 reportWebVitals();
